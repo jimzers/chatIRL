fabba6f8bbfafabad56bb807d69d6e5b40ab994f
jimzers <38634441+jimzers@users.noreply.github.com>
2018-08-23 10:18:38 -0700

    Initial commit

---
+++ login/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import RegexValidator
+
+#
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.TextField(max_length=30, blank=True)
+    last_name = models.TextField(max_length=30, blank=True)
+    email = models.TextField(max_length=30, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
+    # hobbies = []
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
---
+++ matchUser/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MatchuserConfig(AppConfig):
+    name = 'matchUser'
